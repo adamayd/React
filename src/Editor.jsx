965aefd0411ba9cbbbb314542436a711a254d2b6
Adam Ayd <adamayd@gmail.com>
2017-10-21 15:43:11 -0400

    fixed database

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -3,55 +3,55 @@ import React from 'react';
 import database from './database';
 
 const sendMessage = (event) => {
-    event.preventDefault();
-    console.log(event);
+  event.preventDefault();
+  console.log(event);
 }
 
 class Editor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.sendMessage = this.sendMessage.bind(this);
-        this.setValue = this.setValue.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.sendMessage = this.sendMessage.bind(this);
+    this.setValue = this.setValue.bind(this);
+  }
 
-    sendMessage(event) {
-        event.preventDefault();
-        const form = event.target;
+  sendMessage(event) {
+    event.preventDefault();
+    const form = event.target;
 
-        const text = form.text.value;
-        const timestamp = new Date();
-        const name = 'Some Name';
-        const avatarUrl = 'some_url.png';
-        
-        const {addMessage} = this.props;
-        // addMessage({avatarUrl: avatarUrl, name: name, text: text, timestamp: timestamp});
+    const text = form.text.value;
+    const timestamp = new Date();
+    const name = 'Some Name';
+    const avatarUrl = 'some_url.png';
 
-        database.messages.add('general', {avatarUrl: avatarUrl, name: name, text: text, timestamp: timestamp})
+    const {addMessage} = this.props;
+    // addMessage({avatarUrl: avatarUrl, name: name, text: text, timestamp: timestamp});
 
-        this.setState({value: ''});
-    }
+    database.messages.add('general', {avatarUrl: avatarUrl, name: name, text: text, timestamp: timestamp})
 
-    setValue(event) {
-        const input = event.target;
-        const value = input.value;
-        this.setState({value: value});
-    }
+    this.setState({value: ''});
+  }
 
-    render () {
-        const {value} = this.state;
-        return (
-            <form style={styles.editor} onSubmit={this.sendMessage}>
-                <input autoComplete="off" 
-                       style={styles.input} 
-                       name="text" 
-                       type="text" 
-                       value={this.state.value} 
-                       onChange={this.setValue}
-                />
-            </form>
-        );
-    }
+  setValue(event) {
+    const input = event.target;
+    const value = input.value;
+    this.setState({value: value});
+  }
+
+  render () {
+    const {value} = this.state;
+    return (
+      <form style={styles.editor} onSubmit={this.sendMessage}>
+        <input autoComplete="off" 
+               style={styles.input} 
+               name="text" 
+               type="text" 
+               value={this.state.value} 
+               onChange={this.setValue}
+        />
+      </form>
+    );
+  }
 }
 
 const styles = {
