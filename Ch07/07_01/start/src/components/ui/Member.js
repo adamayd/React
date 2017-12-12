d28672335992fe71385c4958a0b8c3253aeeae07
Adam Ayd <adamayd@gmail.com>
2017-12-11 23:26:17 -0500

    Chapter 7 Complete w separate app Course Complete

--- Ch07/07_01/start/src/components/ui/Member.js
+++ Ch07/07_01/start/src/components/ui/Member.js
@@ -3,18 +3,39 @@ import { Component } from 'react'
 
 class Member extends Component {
 
-render() {
-	
-    return (
-        <div className="member">
+    componentWillMount() {
+        this.style={
+            backgroundColor: 'gray'
+        }
+    }
 
-        //
-        // TODO: Create Member Component
-        //
+    componentWillUpdate(nextProps) {
+        this.style = {
+            backgroundColor: (nextProps.admin) ? 'green' : 'purple'
+        }
+    }
 
-        </div>
-    )
-}
+    shouldComponentUpdate(nextProps) {
+        return this.props.admin !== nextProps.admin
+    }
+
+    render() {
+
+        const { name, thumbnail, email, admin, makeAdmin, removeAdmin } = this.props
+        
+        return (
+            <div className="member" style={this.style}>
+                <h1>{name} {(admin) ? <FaShield /> : null}</h1>
+                {(admin) ? 
+                    <a onClick={() => removeAdmin(email)}>Remove Admin</a>
+                :
+                    <a onClick={() => makeAdmin(email)}>Make Admin</a>
+                }
+                <img src={thumbnail} alt="profile picture" />
+                <p><a href={`mailto:${email}`}>{email}</a></p>
+            </div>
+        )
+    }
 }
 
 export default Member
--- Ch07/07_01/start/src/index.js
+++ Ch07/07_01/start/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import routes from './routes'
-import { Member } from './components/ui/Member'
+import routes from './routes'
 
 window.React = React
 
 render(
-	<Member admin={true}
-            name="Edna Welch"
-            email="edna.welch88@example.com"
-            thumbnail="https://randomuser.me/api/portraits/women/90.jpg"
-            makeAdmin={(email) => console.log(email)}/>, 
+	routes, 
 	document.getElementById('react-container'))
