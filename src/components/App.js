ccb5a5b9bf391157983bd73f643947baf3540286
Adam Ayd <adamayd@gmail.com>
2018-03-08 22:41:35 -0500

    added sample fishes to state from file

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   // constructor() {
@@ -20,6 +21,9 @@ class App extends React.Component {
     // 3. Set new fishes object to state
     this.setState({ fishes })
   }
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -27,7 +31,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory 
+          addFish={this.addFish} 
+          loadSampleFishes={this.loadSampleFishes} 
+        />
       </div>
     );
   }
