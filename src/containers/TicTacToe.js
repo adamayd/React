5cb3c271c7bc4788ca432b0a80d8799f70bb87e7
Adam Ayd <adamayd@gmail.com>
2018-04-06 23:30:31 -0400

    added konva to project and moved Home to TicTacToe with stage

---
+++ src/containers/TicTacToe.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Stage } from 'react-konva'
+
+class TicTacToe extends React.Component {
+
+  state = {
+    rows: 3
+  }
+
+  componentWillMount() {
+    let height = window.innerHeight
+    let width = window.innerWidth
+    let size = (height < width) ? height * 0.8 : width * 0.8
+    let rows = this.state.rows
+    let unit = size / rows
+
+    this.setState({
+      size,
+      rows,
+      units
+    })
+  }
+
+  move = () => {
+
+  }
+
+  makeAIMove = () => {
+
+  }
+
+  turingTest = () => {
+
+  }
+
+  recordGame = () => {
+
+  }
+
+  render() {
+      return (
+        <div>
+          <Stage
+            width={}
+            height={}
+          >
+            {/* <Board /> */}
+            {/* <Squares /> */}
+          </Stage>
+        </div>
+      )
+  }
+}
+
+export default TicTacToe
