2bfb5acb303ca449a0297532b5239495f610dad4
Adam Ayd <adamayd@gmail.com>
2018-04-29 22:44:25 -0400

    added react and react-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // console.log(`I'm a silly entry point`);
 
-const arr = [1, 2, 3]
-const iAmJavascriptES6 = () => console.log(...arr)
-window.iAmJavascriptES6 = iAmJavascriptES6
+// const arr = [1, 2, 3]
+// const iAmJavascriptES6 = () => console.log(...arr)
+// window.iAmJavascriptES6 = iAmJavascriptES6
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
