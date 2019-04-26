65dd428c34c6b01cdad8bcb321070068b41a2b13
Adam Ayd <adamayd@gmail.com>
2019-04-25 22:39:32 -0400

    cleaned up for project styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
-
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => props.huge ? '100px' : '50px'};
-  .poop {
-    font-size: 100px;
-  }
-`;
 
 class Page extends Component {
   render() {
@@ -17,17 +8,9 @@ class Page extends Component {
       <div>
         <Meta />
         <Header /> 
-        <MyButton huge>
-          Click Me
-          <span className="poop">💩</span>
-        </MyButton>
-        <MyButton>
-          Click Me
-          <span className="poop">💩</span>
-        </MyButton>
         {this.props.children}
       </div>
-    )
+    );
   }
 }
 
