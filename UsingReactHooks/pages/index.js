bda922f509302d5dcff595a23ae0b7d28737895f
Adam Ayd <adam.ayd@gmail.com>
2019-08-14 23:49:12 -0400

    moved in demo files and create eslintrc

--- UsingReactHooks/pages/index.js
+++ UsingReactHooks/pages/index.js
@@ -1,25 +1,8 @@
-import React, { useState } from 'react';
+import React from "react";
+import App from "../src/App";
 
-const InputElement = () => {
+function index() {
+  return <App pageName="Home" />;
+}
 
-  const [inputText, setInputText] = useState("");
-  const [historyList, setHistoryList] = useState([]);
-
-  return (
-    <div>
-      <input
-        onChange={(e) => { 
-          setInputText(e.target.value);
-          setHistoryList([...historyList, e.target.value]);
-        }}
-        placeholder="Enter Some Text" 
-      />
-      <p>{inputText}</p>
-      <ul>
-        {historyList.map(history => <div>{history}</div>)}
-      </ul>
-    </div>
-  )
-};
-
-export default InputElement;
+export default index;
